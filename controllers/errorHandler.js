b8ec7d4582df5b1bb276e62aaba6416ca4e6fc89
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-06-19 19:34:13 +0300

    fix2

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,9 +1,9 @@
 module.exports.errorHandler = (err, res) => {
   if (err.name === "CastError") {
-    res.status(404).send("Пользователь или карточка не найдены");
+    res.status(404).send({ error: "Пользователь или карточка не найдены" });
   } else if (err.name === "ValidationError") {
-    res.status(400).send("Введены некорректные данные");
+    res.status(400).send({ error: "Введены некорректные данные" });
   } else {
-    res.status(500).send("Ошибка сервера");
+    res.status(500).send({ error: "Ошибка сервера" });
   }
 };
