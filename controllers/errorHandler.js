bfd6e85420504458e185c77db6f015fb1562a22e
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-06-20 11:25:38 +0300

    err handl 3

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -2,9 +2,9 @@ module.exports.errorHandler = (err, res) => {
   if (err.name === "ValidationError") {
     res.status(400).send({ message: "Переданы некорректные данные" });
   } else if (err.name === "CastError") {
-    res.status(404).send({ message: "Пользователь не найден" });
-  } else if (err.message === "noEntry") {
     res.status(400).send({ message: "Пользователь не найден" });
+  } else if (err.message === "noEntry") {
+    res.status(404).send({ message: "Пользователь не найден" });
   } else {
     res.status(500).send({ message: "Ошибка сервера" });
   }
