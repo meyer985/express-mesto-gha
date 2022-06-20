c48f68e5bf38e08ca4d7bf76a79f11ced575b4be
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-06-20 11:14:32 +0300

    err handl 1

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,6 +1,6 @@
 module.exports.errorHandler = (err, res) => {
   if (err.name === "ValidationError") {
-    res.status(400).send("Переданы некорректные данные");
+    res.status(400).send({ message: "Переданы некорректные данные" });
   } else if (err.name === "CastError" || err.message === "noEntry") {
     res.status(404).send({ message: "Пользователь не найден" });
   } else {
