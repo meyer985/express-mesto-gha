e262fe5b749c62fd4fdd07cbaae468995287fa0c
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-06-23 13:43:26 +0300

    add status codes file

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -2,32 +2,32 @@ const {
   BAD_REQUEST_STATUS,
   NOT_FOUND_STATUS,
   SERVER_ERROR,
-} = require("./errorCodes");
+} = require('./errorCodes');
 
 module.exports.userErrorHandler = (err, res) => {
-  if (err.name === "ValidationError") {
+  if (err.name === 'ValidationError') {
     res
       .status(BAD_REQUEST_STATUS)
-      .send({ message: "Переданы некорректные данные пользователя" });
-  } else if (err.name === "CastError") {
-    res.status(BAD_REQUEST_STATUS).send({ message: "Передан невалидный id" });
-  } else if (err.message === "noEntry") {
-    res.status(NOT_FOUND_STATUS).send({ message: "Пользователь не найден" });
+      .send({ message: 'Переданы некорректные данные пользователя' });
+  } else if (err.name === 'CastError') {
+    res.status(BAD_REQUEST_STATUS).send({ message: 'Передан невалидный id' });
+  } else if (err.message === 'noEntry') {
+    res.status(NOT_FOUND_STATUS).send({ message: 'Пользователь не найден' });
   } else {
-    res.status(SERVER_ERROR).send({ message: "Ошибка сервера" });
+    res.status(SERVER_ERROR).send({ message: 'Ошибка сервера' });
   }
 };
 
 module.exports.cardErrorHandler = (err, res) => {
-  if (err.name === "ValidationError") {
+  if (err.name === 'ValidationError') {
     res
       .status(BAD_REQUEST_STATUS)
-      .send({ message: "Переданы некорректные данные при создании карточки" });
-  } else if (err.message === "noCard") {
-    res.status(NOT_FOUND_STATUS).send({ message: "Карточка не найдена" });
-  } else if (err.name === "CastError") {
-    res.status(BAD_REQUEST_STATUS).send({ message: "Передан невалидный id" });
+      .send({ message: 'Переданы некорректные данные при создании карточки' });
+  } else if (err.message === 'noCard') {
+    res.status(NOT_FOUND_STATUS).send({ message: 'Карточка не найдена' });
+  } else if (err.name === 'CastError') {
+    res.status(BAD_REQUEST_STATUS).send({ message: 'Передан невалидный id' });
   } else {
-    res.status(SERVER_ERROR).send({ message: "Ошибка сервера" });
+    res.status(SERVER_ERROR).send({ message: 'Ошибка сервера' });
   }
 };
