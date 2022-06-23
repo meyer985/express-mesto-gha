1ce0c1cacf6cf246cb05374b92aed70f59b4d8b2
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-06-23 13:41:53 +0300

    error status file

---
+++ utils/errorCodes.js
@@ -0,0 +1,3 @@
+module.exports.BAD_REQUEST_STATUS = 400;
+module.exports.NOT_FOUND_STATUS = 404;
+module.exports.SERVER_ERROR = 500;
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,27 +1,33 @@
+const {
+  BAD_REQUEST_STATUS,
+  NOT_FOUND_STATUS,
+  SERVER_ERROR,
+} = require("./errorCodes");
+
 module.exports.userErrorHandler = (err, res) => {
-  if (err.name === 'ValidationError') {
+  if (err.name === "ValidationError") {
     res
-      .status(400)
-      .send({ message: 'Переданы некорректные данные пользователя' });
-  } else if (err.name === 'CastError') {
-    res.status(400).send({ message: 'Передан невалидный id' });
-  } else if (err.message === 'noEntry') {
-    res.status(404).send({ message: 'Пользователь не найден' });
+      .status(BAD_REQUEST_STATUS)
+      .send({ message: "Переданы некорректные данные пользователя" });
+  } else if (err.name === "CastError") {
+    res.status(BAD_REQUEST_STATUS).send({ message: "Передан невалидный id" });
+  } else if (err.message === "noEntry") {
+    res.status(NOT_FOUND_STATUS).send({ message: "Пользователь не найден" });
   } else {
-    res.status(500).send({ message: 'Ошибка сервера' });
+    res.status(SERVER_ERROR).send({ message: "Ошибка сервера" });
   }
 };
 
 module.exports.cardErrorHandler = (err, res) => {
-  if (err.name === 'ValidationError') {
+  if (err.name === "ValidationError") {
     res
-      .status(400)
-      .send({ message: 'Переданы некорректные данные при создании карточки' });
-  } else if (err.message === 'noCard') {
-    res.status(404).send({ message: 'Карточка не найдена' });
-  } else if (err.name === 'CastError') {
-    res.status(400).send({ message: 'Передан невалидный id' });
+      .status(BAD_REQUEST_STATUS)
+      .send({ message: "Переданы некорректные данные при создании карточки" });
+  } else if (err.message === "noCard") {
+    res.status(NOT_FOUND_STATUS).send({ message: "Карточка не найдена" });
+  } else if (err.name === "CastError") {
+    res.status(BAD_REQUEST_STATUS).send({ message: "Передан невалидный id" });
   } else {
-    res.status(500).send({ message: 'Ошибка сервера' });
+    res.status(SERVER_ERROR).send({ message: "Ошибка сервера" });
   }
 };
