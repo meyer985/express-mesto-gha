a02cb5ca3d1d9704714d061393c1d6287ec2d320
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-07-08 00:23:21 +0300

    err handler fix1

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((req, res) => {
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   if (err.name === 'ValidationError') {
     res
       .status(400)
